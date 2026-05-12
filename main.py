133f29edbf6a6f177de4842c593eb29ce65f0b74
Quang Ho <157239q@gmail.com>
2026-05-13 01:04:44 +0700

    Rework result format to make it easier for models to navigate the page

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def text_genFast(text:str) -> str:
         .json()["message"]["content"].strip()
 
 app = web.Flask(__name__)
-ocr = PaddleOCR(use_doc_orientation_classify=False, use_doc_unwarping=False, use_textline_orientation=False, text_recognition_batch_size=1, textline_orientation_batch_size=1, text_det_limit_side_len=1280, text_det_limit_type="max", enable_hpi=False, use_tensorrt=False, precision="fp16")
+ocr = PaddleOCR(use_doc_orientation_classify=False, use_doc_unwarping=False, use_textline_orientation=False, text_recognition_batch_size=1, textline_orientation_batch_size=1, text_det_limit_side_len=2000, text_det_limit_type="max", enable_hpi=False, use_tensorrt=False, precision="fp32")
 
 def doOcr(im):
-    fn = tempfile.mkstemp(suffix=".jpg")[1]; im | toBytes() | file(fn); a = time.time(); d = ocr.predict(input=fn)[0]; b = time.time()
-    t = [d["rec_texts"], d["rec_boxes"], d["rec_scores"]] | T() | ~apply(lambda x1,y1,x2,y2: [(x1+x2)/2, (y1+y2)/2, x2-x1, y2-y1], 1) | filt("x>0.9", 2) | apply(round, 2, ndigits=2) | insert(["text", "cx,cy,w,h", "score"]) | deref()
-    w, h = im | shape(); os.remove(fn); return {"width": w, "height": h, "data": t, "execDuration": b-a}
+    fn = tempfile.mkstemp(suffix=".jpg")[1]; im | toBytes() | file(fn); a = time.time(); d = ocr.predict(input=fn)[0]; b = time.time() #  | filt("x>0.5", 2)
+    t = [d["rec_texts"], d["rec_boxes"], d["rec_scores"]] | T() | ~apply(lambda x1,y1,x2,y2: [x1,y1,x2,y2,(x1+x2)/2, (y1+y2)/2], 1) | apply(round, 2, ndigits=2) | ~apply(lambda t,bb,s: {"text": t, "bbox": bb[:4], "center": bb[4:], "score": s}) | deref()
+    w, h = im | shape(); os.remove(fn); return {"width": w, "height": h, "data": t, "execDuration": b-a, "ver": 2}
 
 @app.route("/receive", methods=["POST"])
 def receive(): return json.dumps(doOcr(request.data | toImg()))
@@ -27,14 +27,19 @@ def postprocess(x): return x.strip("'").strip('"').strip().strip("`").split("\n"
 
 @app.route("/translate", methods=["POST"])
 def translate():
-    im = request.data | toImg(); ocr = doOcr(im)["data"] | cut(0, 1) | ~apply(lambda x,y: [*y,x]) | ~head(1) | aS(list)
+    im = request.data | toImg(); ocr = doOcr(im)["data"]
     prompt1 = "Is this english? ANSWER TRUE/FALSE ONLY\n\n"
     prompt2 = "Translate the text below to english, be as succinct as possible. DO NOT EXPLAIN, DO NOT ADD FILLER TEXT, DO NOT ADD NOTES, EITHER TRANSLATE OR DO NOTHING\n\n"
     for i,o in enumerate(ocr):
-        if hasNonEnglish(o[4]):
-            tf = text_genFast(prompt1 + o[4] + "\n\n") | aS(postprocess)
-            if tf.lower().startswith("f"): o[4] = text_genFast(prompt2 + o[4] + "\n\n") | aS(postprocess)
-    return k1.drawBoxesOnImage(im, ocr, lineSpacing=0) | toBytes()
+        if hasNonEnglish(o["text"]):
+            tf = text_genFast(prompt1 + o["text"] + "\n\n") | aS(postprocess)
+            if tf.lower().startswith("f"): o["text"] = text_genFast(prompt2 + o["text"] + "\n\n") | aS(postprocess)
+    return k1.drawBoxesOnImage(im, transOldDrawBoxesFormat(ocr), lineSpacing=0) | toBytes()
+
+def transOldDrawBoxesFormat(ocr):
+    res = []
+    for o in ocr: bb = o["bbox"]; res.append([*o["center"], bb[2] - bb[0], bb[3] - bb[1], o["text"]])
+    return res
 
 @app.route("/")
 def index(): return "ok"
